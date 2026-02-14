ad42d7581a7019727528bc895bc70593bf17bd7b
RenShadecrest <michaelaaronphoto@gmail.com>
2026-02-13 17:12:29 -0800

    constants

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,10 +6,15 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	const inflationRate = 6.5
+	var investmentAmount float64
+	years, expectedReturnRate := 10.0, 5.5
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Scan(&investmentAmount)
+
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
