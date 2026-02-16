e36533c567f44cfbcb95e825592f39075ee4dca0
RenShadecrest <michaelaaronphoto@gmail.com>
2026-02-16 09:29:24 -0800

    70

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -10,10 +10,11 @@ func main () {
 
 	fmt.Println("Age:", *agePointer)
 
-	adultYears := getAdultYears(agePointer)
-	fmt.Println(adultYears)
+	convertToAdultYears(agePointer)
+	fmt.Println(age)
 }
 
-func getAdultYears(age *int) int {
-	return *age - 18
+func convertToAdultYears(age *int) {
+	// return *age - 18
+	*age = *age - 18
 }
