1a99ea90f9c2861f271a1ba187d677fd632052ac
RenShadecrest <michaelaaronphoto@gmail.com>
2026-02-16 10:56:07 -0800

    86

---
+++ structs/user/user.go
@@ -0,0 +1,36 @@
+package user
+
+import (
+	"fmt"
+	"time"
+	"errors"
+)
+
+type User struct {
+	firstName string
+	lastName string
+	birthdate string
+	createdAt time.Time
+}
+
+func (u *User) OutputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
+func (u *User) ClearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func New(firstName, lastName, birthdate string) ( *User, error) {
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("First name, last name and birthdate are required.")
+	}
+
+	return &User{
+		firstName: firstName,
+		lastName: lastName,
+		birthdate: birthdate,
+		createdAt: time.Now(),
+	}, nil
+}
