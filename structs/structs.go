78e89ceb1ea6c394cc8544f721ec65739b27edae
RenShadecrest <michaelaaronphoto@gmail.com>
2026-02-16 10:04:02 -0800

    77

--- structs/structs.go
+++ structs/structs.go
@@ -13,13 +13,21 @@ type user struct {
 }
 
 func main() {
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser user
+	appUser = user{
+		firstName: userFirstName,
+		lastName: userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
 
 	// ... do something awesome with that gathered data!
 
-	fmt.Println(firstName, lastName, birthdate)
+	fmt.Println(userFirstName, userLastName, userBirthdate)
 }
 
 func outputUserDetails(firstName, lastName, birthdate string) {
