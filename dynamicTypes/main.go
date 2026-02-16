3d390c7495665f53b5abbeaf77c6fc089f8d07ba
RenShadecrest <michaelaaronphoto@gmail.com>
2026-02-16 15:36:00 -0800

    107

--- dynamicTypes/main.go
+++ dynamicTypes/main.go
@@ -1,23 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	result := add(1, 2)
+	fmt.Println(result)
 }
 
-func add(a, b interface{}) interface {
-	aInt, aIsInt := a.(int)
-	bInt, bIsInt := b.(int)
-
-	if aIsInt && bIsInt {
-		return aInt + bInt
-	}
-
-	aFloat, aIsFloat := a.(float64)
-	bFloat, bIsFloat := b.(float64)
-
-	if aIsFloat && bIsFloat {
-		return aFloat + bFloat
-	}
-
-	return nil
+func add[T int | float64 | string](a, b T) T {
+	return a + b
 }
