5dbf88501df6a707c38ecb6a7bd842ca445c0518
RenShadecrest <michaelaaronphoto@gmail.com>
2026-02-16 15:28:41 -0800

    107

---
+++ dynamicTypes/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+
+}
+
+func add(a, b interface{}) interface {
+	aInt, aIsInt := a.(int)
+	bInt, bIsInt := b.(int)
+
+	if aIsInt && bIsInt {
+		return aInt + bInt
+	}
+
+	aFloat, aIsFloat := a.(float64)
+	bFloat, bIsFloat := b.(float64)
+
+	if aIsFloat && bIsFloat {
+		return aFloat + bFloat
+	}
+
+	return nil
+}
