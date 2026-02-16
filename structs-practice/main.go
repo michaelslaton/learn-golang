9e25a88af5b1bdfd23b27214aa7b4facd2a741e5
RenShadecrest <michaelaaronphoto@gmail.com>
2026-02-16 14:03:34 -0800

    102

--- structs-practice/main.go
+++ structs-practice/main.go
@@ -30,26 +30,28 @@ func main(){
 	}
 
 	todo.Display()
-	err = todo.Save()
+	err = saveData(todo)
 	if err != nil {
-		fmt.Println("Saving the todo failed")
 		return
 	}
 
-	fmt.Println("Saving the todo succeeded.")
-
 	userNote.Display()
-	err = userNote.Save()
+	err = saveData(userNote)
+	if err != nil {
+		return
+	}
+	
+}
+
+func saveData(data saver) error {
+	err := data.Save()
 	if err != nil {
 		fmt.Println("Saving the note failed")
-		return
+		return err
 	}
 
 	fmt.Println("Saving the note succeeded.")
-}
-
-func saveData(data note.Note){
-
+	return nil
 }
 
 func getNoteData() (string, string) {
