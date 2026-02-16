c5e9a03b88a7738c2990213d61053ad1417c41a8
RenShadecrest <michaelaaronphoto@gmail.com>
2026-02-16 12:11:04 -0800

    91

--- structs-practice/main.go
+++ structs-practice/main.go
@@ -3,37 +3,31 @@ package main
 import (
 	"errors"
 	"fmt"
+	"example/note/note"
 )
 
 func main(){
-	title, content, err := getNoteData()
-	
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 }
 
-func getNoteData() (string, string, error) {
-	title, err := getUserInput("Note title:")
-	if err != nil {
-		return "", "", err
-	}
-	content, err := getUserInput("Note content:")
-	if err != nil {
-		return "", "", err
-	}
-	return title, content, nil
+func getNoteData() (string, string) {
+	title := getUserInput("Note title:")
+	content := getUserInput("Note content:")
+	return title, content
 }
 
-func getUserInput(prompt string) (string, error) {
+func getUserInput(prompt string) string {
 	fmt.Print(prompt)
 	var value string
 	fmt.Scanln(&value)
 
-	if value == "" {
-		return "", errors.New("Invalid input.")
-	}
-
-	return value, nil
+	return value
 }
