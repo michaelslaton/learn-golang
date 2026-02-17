993d18bd3c13226d69ec85c618f95f28f477ab89
RenShadecrest <michaelaaronphoto@gmail.com>
2026-02-16 17:22:53 -0800

    120

--- lists/main.go
+++ lists/main.go
@@ -4,8 +4,15 @@ import "fmt"
 
 func main() {
 	websites := map[string]string{
-		"google":              "http://google.com",
+		"Google":              "http://google.com",
 		"Amazon Web Services": "http://aws.com",
 	}
+	fmt.Println(websites)
+	fmt.Println(websites["Amazon Web Services"])
+
+	websites["LinkedIn"] = "http://linkedin.com"
+	fmt.Println(websites)
+
+	delete(websites, "Google")
 	fmt.Println(websites)
 }
