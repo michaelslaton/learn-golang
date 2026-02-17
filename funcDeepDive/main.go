f2dc78976126e87aab253091d17286ba1361f912
michaelslaton <michaelaaronphoto@gmail.com>
2026-02-17 13:26:02 -0800

    132

--- funcDeepDive/main.go
+++ funcDeepDive/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	// numbers := []int{1,10,15}
+	sum := sumup(1,10,15, -5)
+
+	fmt.Println(sum)
+
+}
+
+func sumup(startingValue int, numbers ...int) int {
+	sum := 0
+
+	for _, v := range numbers {
+		sum += v
+	}
+
+	return sum
 }
