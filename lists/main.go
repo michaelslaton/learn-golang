36188f8c45eb17b52cc5715671fbe51564b258eb
RenShadecrest <michaelaaronphoto@gmail.com>
2026-02-16 17:42:05 -0800

    124

--- lists/main.go
+++ lists/main.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
-// import "fmt"
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
 
 func main(){
 	userNames := make([]string, 2, 5)
@@ -15,9 +19,9 @@ func main(){
 
 	fmt.Println(userNames)
 
-	courseRatings := make(map[string]float64, 3)
+	courseRatings := make(floatMap, 3)
 	courseRatings["go"] = 4.7
 	courseRatings["react"] = 4.8
 	courseRatings["angular"] = 4.7
-	fmt.Println(courseRatings)
+	courseRatings.output()
 }
